c30114b1c7528b8871a2fed6138184711ef40f32
WhiteCmile <whitecmile@WhiteCmile>
2024-05-16 14:11:51 +0800

    modify build.py

--- build/build.py
+++ build/build.py
@@ -93,6 +93,60 @@ compile_pass = [
   generate-x86-64
 ))
 """,
+
+# a7
+"""
+(compiler-passes '(
+  verify-scheme
+  remove-complex-opera*
+  flatten-set!
+  impose-calling-conventions
+  uncover-frame-conflict
+  pre-assign-frame
+  assign-new-frame
+  (iterate
+    finalize-frame-locations
+    select-instructions
+    uncover-register-conflict
+    assign-registers
+    (break when everybody-home?)
+    assign-frame)
+  discard-call-live
+  finalize-locations
+  expose-frame-var
+  expose-basic-blocks
+  flatten-program
+  generate-x86-64
+))
+""",
+
+# a8
+"""
+(compiler-passes '(
+  verify-uil
+  remove-complex-opera*
+  flatten-set!
+  impose-calling-conventions
+  expose-allocation-pointer
+  uncover-frame-conflict
+  pre-assign-frame
+  assign-new-frame
+  (iterate
+    finalize-frame-locations
+    select-instructions
+    uncover-register-conflict
+    assign-registers
+    (break when everybody-home?)
+    assign-frame)
+  discard-call-live
+  finalize-locations
+  expose-frame-var
+  expose-memory-operands
+  expose-basic-blocks
+  flatten-program
+  generate-x86-64
+))
+"""
 ]
 
 def main():
